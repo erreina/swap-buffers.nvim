3c775530e667f70dfa769d15029929829250d6af
Ernesto Rodriguez Reina <erreina@gmail.com>
2025-01-15 15:22:42 -0500

    Initial implementation

---
+++ lua/swap-buffers.lua
@@ -0,0 +1,267 @@
+-- A simple NeoVim plugin for swapping windows
+local api = vim.api
+local fn = vim.fn
+local M = {}
+
+-- The keycode for the Escape key, used to cancel the window picker.
+local escape = 27
+
+-- For the sake of keeping this plugin simple, we don't support changing the
+-- dimensions of the floating window.
+local float_height = 3
+local float_width = 6
+
+local config = {
+  -- The characters available for hinting windows.
+  chars = {
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  },
+
+  -- A group to use for overwriting the Normal highlight group in the floating
+  -- window. This can be used to change the background color.
+  normal_hl = 'Normal',
+
+  -- The highlight group to apply to the line that contains the hint characters.
+  -- This is used to make them stand out more.
+  hint_hl = 'Bold',
+
+  -- The border style to use for the floating window.
+  border = 'single',
+
+  -- How the hints should be rendered. The possible values are:
+  --
+  -- - "float" (default): renders the hints using floating windows
+  -- - "status": renders the hints to a string and calls `redrawstatus`,
+  --   allowing you to show the hints in a status or winbar line
+  render = 'float',
+}
+
+local hints = {}
+
+-- Returns a table that maps the hint keys to their corresponding windows.
+local function get_windows_keys(windows)
+  local mapping = {}
+  local chars = config.chars
+  local nrs = {}
+  local ids = {}
+  local current = api.nvim_win_get_number(api.nvim_get_current_win())
+
+  -- We use the window number (not the ID) as these are more consistent. This in
+  -- turn should result in a more consistent choice of window keys.
+  for _, win in ipairs(windows) do
+    local nr = api.nvim_win_get_number(win)
+
+    table.insert(nrs, nr)
+    ids[nr] = win
+  end
+
+  table.sort(nrs)
+
+  local index = 1
+
+  for _, nr in ipairs(nrs) do
+    -- We skip the current window here so that we still "reserve" it the
+    -- character, but don't include it in the output. This ensures that window X
+    -- always gets hint Y, regardless of what the current active window is.
+    if nr ~= current then
+      local key = chars[index]
+
+      if mapping[key] then
+        key = key .. (index == #chars and chars[1] or chars[index + 1])
+      end
+
+      mapping[key] = ids[nr]
+    end
+
+    index = index == #chars and 1 or index + 1
+  end
+
+  return mapping
+end
+
+-- Opens all the floating windows in (roughly) the middle of every window.
+local function open_floats(mapping)
+  local floats = {}
+
+  for key, window in pairs(mapping) do
+    local bufnr = api.nvim_create_buf(false, true)
+
+    if bufnr > 0 then
+      local win_width = api.nvim_win_get_width(window)
+      local win_height = api.nvim_win_get_height(window)
+
+      local row = math.max(0, math.floor((win_height / 2) - 1))
+      local col = math.max(0, math.floor((win_width / 2) - float_width))
+
+      api.nvim_buf_set_lines(
+        bufnr,
+        0,
+        -1,
+        true,
+        { '', '  ' .. key .. '  ', '' }
+      )
+      api.nvim_buf_add_highlight(bufnr, 0, config.hint_hl, 1, 0, -1)
+
+      local float_window = api.nvim_open_win(bufnr, false, {
+        relative = 'win',
+        win = window,
+        row = row,
+        col = col,
+        width = #key == 1 and float_width - 1 or float_width,
+        height = float_height,
+        focusable = false,
+        style = 'minimal',
+        border = config.border,
+        noautocmd = true,
+      })
+
+      api.nvim_set_option_value(
+        'winhl',
+        'Normal:' .. config.normal_hl,
+        { win = float_window }
+      )
+      api.nvim_set_option_value('diff', false, { win = float_window })
+
+      floats[float_window] = bufnr
+    end
+  end
+
+  -- We need to redraw here, otherwise the floats won't show up
+  vim.cmd('redraw')
+
+  return floats
+end
+
+local function close_floats(floats)
+  for window, bufnr in pairs(floats) do
+    api.nvim_win_close(window, true)
+    api.nvim_buf_delete(bufnr, { force = true })
+  end
+end
+
+local function get_char()
+  local ok, char = pcall(fn.getchar)
+
+  return ok and fn.nr2char(char) or nil
+end
+
+local function show_hints(keys, redraw)
+  if config.render == 'status' then
+    for key, win in pairs(keys) do
+      hints[win] = key
+    end
+
+    if redraw then
+      vim.cmd('redrawstatus!')
+    end
+  else
+    return open_floats(keys)
+  end
+end
+
+local function hide_hints(state, redraw)
+  if config.render == 'status' then
+    hints = {}
+
+    if redraw then
+      vim.cmd('redrawstatus!')
+    end
+  else
+    close_floats(state)
+  end
+end
+
+-- Returns the hint character(s) for the given window, or `nil` if there aren't
+-- any.
+--
+-- This method only returns a value if the `render` option is set to `status`.
+function M.hint(window)
+  return hints[window]
+end
+
+-- Configures the plugin by merging the given settings into the default ones.
+function M.setup(user_config)
+  config = vim.tbl_extend('force', config, user_config)
+end
+
+-- Picks a window to swap with
+function M.pick()
+  local windows = vim.tbl_filter(function(id)
+    return api.nvim_win_get_config(id).relative == ''
+  end, api.nvim_tabpage_list_wins(0))
+
+  local window_keys = get_windows_keys(windows)
+  local hints_state = show_hints(window_keys, true)
+  local key = get_char()
+
+  if not key or key == escape then
+    hide_hints(hints_state, true)
+    return
+  end
+
+  local target_window = window_keys[key]
+  local extra = {}
+  local choices = 0
+
+  for hint, win in pairs(window_keys) do
+    if vim.startswith(hint, key) then
+      extra[hint] = win
+      choices = choices + 1
+    end
+  end
+
+  if choices > 1 then
+    hide_hints(hints_state, false)
+    hints_state = show_hints(extra, true)
+
+    local second = get_char()
+
+    if second then
+      local combined = key .. second
+
+      target_window = window_keys[combined] or window_keys[key]
+    else
+      target_window = nil
+    end
+  end
+
+  hide_hints(hints_state, true)
+
+  if target_window then
+    local current_buffer = vim.fn.bufnr()
+
+    local target_window_id = vim.fn.win_getid(target_window)
+    local target_buffer = vim.fn.bufnr(target_window_id)
+
+    vim.cmd("b " .. target_buffer)
+    vim.fn.win_gotoid(target_window_id)
+    vim.cmd("b " .. current_buffer)
+  end
+end
+
+return M
